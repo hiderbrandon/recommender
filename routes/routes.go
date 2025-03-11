948d27f44aae50dc29b1bb372e86f7b295a6f155
hiderbrandon <hiderbrandon@gmail.com>
2025-03-11 00:40:48 -0500

    added CORS config to allow dev test on front

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,28 @@
 package routes
 
 import (
+	"log"
 	"recommender/internal/adapters/handlers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(stockHandler *handlers.StockHandler) *gin.Engine {
 	r := gin.Default()
 
+	// Configurar CORS para aceptar cualquier origen
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // 🔥 Permitir cualquier origen
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false, // No permitir credenciales por seguridad
+	}))
+
+	log.Println("✅ CORS configurado para permitir cualquier origen.")
+
+	// Definir rutas
 	r.GET("/stocks", stockHandler.GetStocks)
 	r.POST("/stocks", stockHandler.PostStock)
 	r.GET("/stocks/recommendations", stockHandler.GetRecommendations)
